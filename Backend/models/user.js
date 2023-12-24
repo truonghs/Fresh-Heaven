5ab6a5b9f07d24319bac32898609f8aff2656cd8
Devbeee <tngb1002@gmail.com>
2023-12-24 10:58:50 +0700

    add field in user model

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,43 +1,64 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default: null,
+  },
+  firstName: {
+    type: String,
+    default: "",
+  },
+  lastName: {
+    type: String,
+    default: "",
+  },
+  phoneNumber: {
+    type: String,
+    default: "",
+  },
+  paymentMethod: {
+    type: String,
+    default: "",
+  },
+  addresses: [
+    // location: {
+    //   type: String,
+    //   default: '',
+    // },
+  ],
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: String,
+
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
     },
-    avt: String,
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    verified: {
-        type: Boolean,
-        default: false,
-    },
-    verificationToken: String,
-    addresses: [
-        {
-            name: String,
-            phoneNumber: String,
-            houseNumber: String,
-            detail: String,
-            city: String,
-        },
-    ],
-    orders: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Order",
-        },
-    ],
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  ],
+  firstTime: {
+    type: Boolean,
+    default: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 module.exports = mongoose.model("User", userSchema);
